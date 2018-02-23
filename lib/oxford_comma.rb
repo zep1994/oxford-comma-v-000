9518b191bc37e07f150de0ccd227af3a736b4893
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-02-23 00:14:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+    elsif array.length == 2
+     return array.join(" and ")
+    elsif array.length == 3
+   array[0..-2].join(", ") + "," + " and " + array[2]
+ elsif array.length > 3
+  array.insert(-2, "and").join(", ").sub("and,", "and")
+  end
 end
